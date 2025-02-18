812c24f2db1813718b30d519cc9e5f969e2a8500
Jinansh <149217594+Jinansh230705@users.noreply.github.com>
2025-02-18 15:22:24 +0530

    pc

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -1,24 +1,31 @@
 exports.handler = async (event) => {
     const queryString = event.queryStringParameters;
-    
-    const allowedRef = "boxit_pc"; 
+    const cookies = event.headers.cookie || "";
+    const allowedRef = "boxit_pc";
+
+    if (cookies.includes("access_granted=true")) {
+        return {
+            statusCode: 200,
+            body: "Authenticated. Continue to page.",
+        };
+    }
 
     if (queryString.ref === allowedRef) {
         return {
-            statusCode: 302, 
+            statusCode: 302,
             headers: {
-                Location: "/p", 
+                "Set-Cookie": "access_granted=true; Path=/; HttpOnly",
+                "Location": "/p", 
             },
             body: "Redirecting...",
         };
     }
 
     return {
-        statusCode: 302,
+        statusCode: 403,
         headers: {
             Location: "/403",
         },
         body: "Redirecting...",
     };
-    
 };
